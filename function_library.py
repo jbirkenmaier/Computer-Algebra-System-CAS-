936945802a0006aae6cc258709f755c831ee3465
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2024-03-26 12:13:14 +0100

    update

--- function_library.py
+++ function_library.py
@@ -20,14 +20,15 @@ class Equat_object: #math objects can be for example equations
     def interpretation_of_string_input(self):
         for position, element in enumerate(self.equation_string):   #the attributes of the math objects are variables, constants and mathematical operators
             if element in math_operators:
-                element = Math_operator('math_op',element,position)
+                element = Math_operator('math_op',element,position,self.object_family, self.equation_string)
                 self.equation_interpretation.append(element)
                 
 class Math_operator(Equat_object):
     #The math operator is now a subclass of Equat_object.
     #The math operator belongs to the object family, for example "equation1"
     #We now have to give this object the corresponding mathematical symbol, and save its position in the string
-    def __init__(self, obj_type, element_str, position_in_string):
+    def __init__(self, obj_type, element_str, position_in_string,object_family, equation_string):
+        super().__init__(object_family, equation_string)
         self.obj_type = 'math_op'
         self.symbol = element_str
         self.position_in_string = position_in_string
--- main.py
+++ main.py
@@ -10,4 +10,8 @@ e.interpretation_of_string_input() #interpreting the string input
 #now I want to print out math operators and their positions
 
 math_object = e.equation_interpretation[0]
-print(math_object.obj_type,math_object.symbol, math_object.position_in_string)
+print(math_object.obj_type)
+print(math_object.symbol)
+print(math_object.position_in_string)
+print(math_object.object_family)
+print(math_object.equation_string)
