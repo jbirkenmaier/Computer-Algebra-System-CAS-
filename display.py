494d285ade74037373e692456bc5841d154132ab
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2024-03-27 09:06:36 +0100

    update

--- display.py
+++ display.py
@@ -1,4 +1,21 @@
 import function_library as fl
-from tkinter import *
+import tkinter as tk
+
+root = tk.Tk() #create a window
+root.title("Input Window")
+root.geometry("1000x1000")
+
+def printInput(): 
+    inp = inputtxt.get("end-1c linestart", "end-1c lineend")
+    label = tk.Label(root, text = inp)
+    label.pack()
+
+def on_enter(event):
+    printInput()
+
+inputtxt = tk.Text(root, height = 20, width = 150) 
+inputtxt.pack()
+inputtxt.bind("<Return>",on_enter)
 
 
+root.mainloop()
